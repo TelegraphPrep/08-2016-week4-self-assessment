25450f9d6a0dc59c5041b4fccd6d9283d9abb988
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-09-01 19:56:56 -0700

    Extend Obj solution

--- extend-obj/extendObj.js
+++ extend-obj/extendObj.js
@@ -1,11 +1,21 @@
 // define loop here
-
+var loop = function(object, callback) {
+  for (var key in object) {
+    callback(object[key], key);
+  }
+};
 
 // define extendObj here
-
+var extendObj = function(object, objectExtendBy) {
+  loop(objectExtendBy, function(value, key) {
+    object[key] = value;
+  });
+};
 
 // test data we're giving you:
 var Jon = {name: "Jon", fear: "koala bears"};
 var moreCharacteristics = {favoriteFruit: "avocados", funFact: "I once played basketball with Prince in the summer of 1984."};
 
 // test extendObj here using the two sample data objects given above
+extendObj(Jon, moreCharacteristics);
+console.log("Jon: ", Jon);
